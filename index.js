3d3ddd958dd5be663c801ca74dc3b3d73b4e29a4
Jonathan Reisdorf <fishlevel@crossnode.org>
2018-02-18 14:31:18 +0100

    hotfix: automatically adjust nonce to time expected by server in case of error

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class Kucoin {
   rawRequest(method, endpoint, signed = false, params) {
     let deferred = Q.defer()
     let path = this.path_prefix + endpoint
-    let nonce = new Date().getTime()
+    let nonce = new Date().getTime() + (this._timeAdjust || 0)
     let queryString
     if (params !== undefined) {
       queryString = [];
@@ -75,6 +75,8 @@ class Kucoin {
           if (!err && !obj.success) {
             err = obj
           }
+
+          this._timeAdjust = err && err.body && err.body.timestamp && err.body.timestamp - Date.now() || 0
           deferred.reject(err)
         } else {
           deferred.resolve(obj)
